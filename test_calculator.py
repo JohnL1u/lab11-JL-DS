095b2c95d910e3f140327f5b658e60648dd5a0af
danichaa26 <danichaa26>
2025-11-12 18:03:35 -0500

    unittest changes

--- test_calculator.py
+++ test_calculator.py
@@ -2,13 +2,16 @@ import unittest
 from calculator import *
 
 class TestCalculator(unittest.TestCase):
-    ######### Partner 2
-    # def test_add(self): # 3 assertions
-    #     fill in code
+    def test_add(self): # 3 assertions
+        self.assertEqual(add(1, 2), 3)
+        self.assertEqual(add(1, 0), 1)
+        self.assertEqual(add(0, 1), 1)
+        self.assertEqual(add(0, 0), 0)
 
-    # def test_subtract(self): # 3 assertions
-    #     fill in code
-    # ##########################
+    def test_subtract(self): # 3 assertions
+        self.assertEqual(sub(3, 2), 1)
+        self.assertEqual(sub(5, 0), 5)
+        self.assertEqual(sub(0, 0), 0)
 
     ######## Partner 1
     # def test_multiply(self): # 3 assertions
@@ -18,20 +21,21 @@ class TestCalculator(unittest.TestCase):
     #     fill in code
     # ##########################
 
-    ######## Partner 2
-    # def test_divide_by_zero(self): # 1 assertion
-    #     # call division function inside, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #     div(0, 5)
-    #     fill in code
+    def test_divide_by_zero(self): # 1 assertion
+        with self.assertRaises(ZeroDivisionError):
+            div(0, 5)
 
-    # def test_logarithm(self): # 3 assertions
-    #     fill in code
+    def test_logarithm(self): # 3 assertions
+        self.assertEqual(log(16, 2), 4.0)
+        self.assertEqual(log(9, 3), 2.0)
 
-    # def test_log_invalid_base(self): # 1 assertion
-    #     # use same technique from test_divide_by_zero
-    #     fill in code
-    # ##########################
+    def test_log_invalid_base(self): # 1 assertion
+        with self.assertRaises(ValueError):
+            log(0, 10)
+            log(-2, 4)
+            log(10, 0)
+            log(7, -4)
+            log(8, 1)
     
     ######## Partner 1
     # def test_log_invalid_argument(self): # 1 assertion
