19133b1f57502a00cbd8578070cea4245fd5f532
CiEn Liu <johnliusmac@CiEndeMacBook-Air.local>
2025-11-12 16:42:51 -0500

    modified calculator p1

--- calculator.py
+++ calculator.py
@@ -4,8 +4,31 @@ calculator.py
 
 One function per operation, in order.
 """
-# First example
-def add(a, b): 
-    pass
 
+import math
 
+def add(a, b):
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+def mul(a, b):
+    return a * b
+
+def div(a, b):
+    if a == 0:
+        raise ZeroDivisionError("division by zero: 'a' is 0 in b / a")
+    return b / a
+
+def log(a, b):
+    if a <= 0:
+        raise ValueError("log base must be positive")
+    if a == 1:
+        raise ValueError("log base cannot be 1")
+    if b <= 0:
+        raise ValueError("log argument must be positive")
+    return math.log(b, a)
+
+def exp(a, b):
+    return a ** b
